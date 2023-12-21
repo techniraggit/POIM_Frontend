aadadc6c391f7f5300f86d81eab75eb867ba416f
kavitanirag <kavita.thakur@niraginfotech.com>
2023-12-21 18:40:35 +0530

    feat(adduse):add user

--- pages/add-user.jsx
+++ pages/add-user.jsx
@@ -42,12 +42,12 @@ const AddUser = ({ base_url }) => {
       const data = {
         ...values,
       }
-     
-        const response = await axios.post(`${base_url}/api/admin/users`, data, {
-          headers: headers,
-        });
-        console.log(response.data.message,'jjjjjjjjjjjjjj');
-        if(response.status==true){
+
+      const response = await axios.post(`${base_url}/api/admin/users`, data, {
+        headers: headers,
+      });
+      console.log(response.data.message, 'jjjjjjjjjjjjjj');
+      if (response.status == true) {
         message.success(response.data.message);
         router.push('/user-list')
       }
@@ -66,7 +66,7 @@ const AddUser = ({ base_url }) => {
     // axios.post(`${base_url}/api/accounts/forget-password`)
     // Handle form submission here
   };
- 
+
   return (
     <>
       <DynamicTitle title="Add User" />
@@ -81,88 +81,153 @@ const AddUser = ({ base_url }) => {
                 <span>Edit User</span>
               </li>
             </ul>
-            <Form onFinish={onFinish} layout="vertical"
-              labelCol={{ span: 8 }}
-              wrapperCol={{ span: 16 }}
-            >
-              <div className="row">
-                {/* <div className="Role">
+            <div class="choose-potype round-wrap"><div class="inner-choose">
+              <Form onFinish={onFinish} layout="vertical"
+                labelCol={{ span: 8 }}
+                wrapperCol={{ span: 16 }}
+              >
+                <div className="row">
+                  {/* <div className="Role">
                   <p>Role</p>
                 </div> */}
-                <Form.Item label="Role" name="role_id" initialValue="select role" className='dropdown vender-input'>
-                  <Select >
-                    {Array.isArray(roles) &&
-                      roles.map((role) => (
-                        <Option key={role.id} value={role.id}
-                        >
-                          {role.name}
-                        </Option>
-                      ))}
-                  </Select>
-                </Form.Item>
-                <Form.Item
-                  label="First Name"
-                  name="first_name"
-                  // Add a name to link the input to the form values
-                  className="vender-input"
-                  rules={[{ required: true, message: 'Please enter your first name!' }]}
-                >
-                  <Input />
-                </Form.Item>
-                <Form.Item
-                  label="Last Name"
-                  name="last_name"  // Add a name to link the input to the form values
-                  className="vender-input"
-                  rules={[{ required: true, message: 'Please enter your last name!' }]}
-                >
-                  <Input />
-                </Form.Item>
-                <Form.Item
-                  label="Email Address"
-                  name="email"  // Add a name to link the input to the form values
-                  className="vender-input"
-                  rules={[{ required: true, message: 'Please enter your email address!' }]}
-                >
-                  <Input />
-                </Form.Item>
-                <Form.Item
-                  label="Contact Number"
-                  name="phone_number"  // Add a name to link the input to the form values
-                  className="vender-input"
-                  rules={[{ required: true, message: 'Please enter your contact number !' }]}
-                >
-                  <Input />
-                </Form.Item>
-                <Form.Item
-                  label="Address"
-                  name="address"  // Add a name to link the input to the form values
-                  className="vender-input"
-                  rules={[{ required: true, message: 'Please enter your address !' }]}
-                >
-                  <Input />
-                </Form.Item>
-                <Form.Item
-                  label="State / Province"
-                  name="state"  // Add a name to link the input to the form values
-                  className="vender-input"
-                  rules={[{ required: true, message: 'Please enter your state!' }]}
-                >
-                  <Input />
-                </Form.Item>
-                <Form.Item
-                  label="Country"
-                  name="country"  // Add a name to link the input to the form values
-                  className="vender-input"
-                  rules={[{ required: true, message: 'Please enter your country!' }]}
-                >
-                  <Input />
-                </Form.Item>
-                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                  <button type="submit" className="create-ven-butt">Submit</button>
-                </Form.Item>
-              </div>
-            </Form>
-            
+                  <div className="col-lg-4 col-md-6">
+                    <div className="selectwrap react-select">
+
+                    <Form.Item label="Role" name="role_id" initialValue="select role" className='dropdown vender-input'>
+                      <Select >
+                        {Array.isArray(roles) &&
+                          roles.map((role) => (
+                            <Option key={role.id} value={role.id}
+                            >
+                              {role.name}
+                            </Option>
+                          ))}
+                      </Select>
+                    </Form.Item>
+                    </div>
+                  </div>
+                </div>
+                <div className="row">
+
+                  <div className="col-lg-4 col-md-6">
+                    <div class="wrap-box">
+                      <Form.Item
+                        label="First Name"
+                        name="first_name"
+                        // Add a name to link the input to the form values
+                        className="vender-input"
+                        rules={[{ required: true, message: 'Please enter your first name!' }]}
+                      >
+                        <Input />
+                      </Form.Item>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-md-6">
+                    <div class="wrap-box">
+                      <Form.Item
+                        label="Last Name"
+                        name="last_name"  // Add a name to link the input to the form values
+                        className="vender-input"
+                        rules={[{ required: true, message: 'Please enter your last name!' }]}
+                      >
+                        <Input />
+                      </Form.Item>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-md-6">
+                    <div class="wrap-box">
+                      <Form.Item
+                        label="Email Address"
+                        name="email"  // Add a name to link the input to the form values
+                        className="vender-input"
+                        rules={[{ required: true, message: 'Please enter your email address!' }]}
+                      >
+                        <Input />
+                      </Form.Item>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-md-6">
+                    <div class="wrap-box">
+                      <Form.Item
+                        label="Contact No"
+                        name="phone_number"  // Add a name to link the input to the form values
+                        className="vender-input"
+                        rules={[{ required: true, message: 'Please enter your contact number !' }]}
+                      >
+                        <Input />
+                      </Form.Item>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-md-6">
+                    <div class="wrap-box">
+                      <Form.Item
+                        label="Edit Password"
+                        name="address"  // Add a name to link the input to the form values
+                        className="vender-input"
+                        rules={[{ required: true, message: 'Please enter your address !' }]}
+                      >
+                        <Input />
+                      </Form.Item>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-md-6">
+                    <div class="wrap-box">
+                      <Form.Item
+                        label="Confirm New Password"
+                        name="state"  // Add a name to link the input to the form values
+                        className="vender-input"
+                        rules={[{ required: true, message: 'Please enter your state!' }]}
+                      >
+                        <Input />
+                      </Form.Item>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-md-6">
+                    <div class="wrap-box">
+                      <Form.Item
+                        label="Address"
+                        name="country"  // Add a name to link the input to the form values
+                        className="vender-input"
+                        rules={[{ required: true, message: 'Please enter your country!' }]}
+                      >
+                        <Input />
+                      </Form.Item>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-md-6">
+                    <div class="wrap-box">
+                      <Form.Item
+                        label="State / Province"
+                        name="country"  // Add a name to link the input to the form values
+                        className="vender-input"
+                        rules={[{ required: true, message: 'Please enter your country!' }]}
+                      >
+                        <Input />
+                      </Form.Item>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-md-6">
+                    <div class="wrap-box">
+                      <Form.Item
+                        label="Country"
+                        name="country"  // Add a name to link the input to the form values
+                        className="vender-input"
+                        rules={[{ required: true, message: 'Please enter your country!' }]}
+                      >
+                        <Input />
+                      </Form.Item>
+                    </div>
+                  </div>
+                  <div className="col-12">
+                    <Form.Item >
+                      <button type="submit" className="create-ven-butt">Submit</button>
+                    </Form.Item>
+                  </div>
+                </div>
+              </Form>
+            </div>
+            </div>
+
           </div>
         </div>
       </div>
