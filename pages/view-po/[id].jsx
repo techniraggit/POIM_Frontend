83337a37c904bedb3b8816d315fb279c34a96025
shanunirag <shanu.sharma@niraginfotech.com>
2024-01-31 19:08:10 +0530

    bug fix

--- pages/view-po/[id].jsx
+++ pages/view-po/[id].jsx
@@ -111,8 +111,14 @@ const ViewPO = () => {
                 form.setFieldValue('material_delivery', data.material_details[0]?.delivery_address || '1860 Shawson')
                 form.setFieldValue('first_name', data.created_by.first_name)
                 form.setFieldValue('last_name', data.created_by.last_name)
+                // data?.material_details.forEach((material, index) => {
+                //     form.setFieldValue(('project_site_id' + (index)), material.project_site?.site_id)
+                // })
                 data?.material_details.forEach((material, index) => {
-                    form.setFieldValue(('project_site_id' + (index)), material.project_site?.site_id)
+                    form.setFieldValue('project_site_id' + (index), material.project_site?.site_id)
+                    form.setFieldValue('material_for' + (index), material.material_for)
+                    form.setFieldValue('project_id' + (index), material.project?.project_id)
+                    form.setFieldValue('project_site_id' + (index), material.project_site?.site_id)
                 })
             }
         });
