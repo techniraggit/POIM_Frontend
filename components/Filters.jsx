0f2f1c2a1add5de2b2f2b77df1414e3f0a6c6b2e
Rajwinder Singh <rajwinders474@gmail.com>
2024-02-26 16:26:15 +0530

    filter design changes

--- components/Filters.jsx
+++ components/Filters.jsx
@@ -60,8 +60,8 @@ const Filters = ({ search, toDate, fromDate, role, type, status, vendor, downloa
     }
 
   return (
-    <div className="searchbar-wrapper">
-        <div className="Purchase-form">
+    <div className="filter-wrapper">
+        <div className="filter-main">
             <div className="wrapin-form add-clear-wrap mt-0">
                 {search && <input className="vendor-input" placeholder="Search Invoice"
                     value={inputValue} onChange={(event) => setInputValue(event.target.value)}
