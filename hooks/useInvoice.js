703ea7b07b61ac18c9f0b17a4e92373081b93703
shanunirag <shanu.sharma@niraginfotech.com>
2024-03-06 12:07:01 +0530

    pm button

--- hooks/useInvoice.js
+++ hooks/useInvoice.js
@@ -8,10 +8,11 @@ const useInvoice = (invoice) => {
   if(invoice?.purchase_order?.po_type === 'material' || invoice?.purchase_order?.po_type === 'rental' 
   || invoice?.purchase_order?.po_type === 'subcontractor'
   ) {
-    if(invoice?.po_creator) {
+    if(invoice?.po_creator && user?.role !== 'project manager') {
         approval_enabled = invoice?.po_creator_approval_status === 'pending';
     } else if(user?.role === 'project manager') {
         approval_enabled = invoice?.po_creator_approval_status === 'approved' && invoice?.pm_approval_status  === 'pending';
+        
     } else if(user?.role === 'department manager') {
         approval_enabled = invoice?.po_creator_approval_status === 'approved' && invoice?.pm_approval_status === 'approved' && invoice?.dm_approval_status === 'pending';
     }
