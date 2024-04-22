42357f340335674dee27f8b2fea231063baf89e4
shanu sharma <shanu.sharma@nirag.com>
2024-04-22 14:26:52 +0530

    changes

--- components/Filters.jsx
+++ components/Filters.jsx
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { Button, Select } from 'antd';
-import { fetchVendorContact, searchUserRoles, searchPoName } from "@/apis/apis/adminApis";
+import { fetchVendorContact, searchUserRoles, searchPoName, fetchProjects } from "@/apis/apis/adminApis";
 const { Option } = Select;
 
-const Filters = ({ search, name, toDate, fromDate, role, type, status, vendor, download, applyFilters, currentPage, downloadPdf }) => {
+const Filters = ({ search, name, toDate, fromDate, role, type, status, vendor, project_number, download, applyFilters, currentPage, downloadPdf }) => {
     const [inputValue, setInputValue] = useState('');
     const [companyName, setCompanyName] = useState([]);
     const [roleName, setRoleName] = useState([]);
+    const [projects, setProjects] = useState([]);
     const [errorMessage, setErrorMessage] = useState("");
     const [poName, setPoName] = useState([]);
     const [query, setQuery] = useState({
         filter_by_po_type: "",
         filter_by_po_vendor: "",
         filter_by_po_status: "",
+        filter_by_project_number: "",
         filter_by_role: "",
         filter_by_name: "",
         to_date: "",
@@ -36,6 +38,19 @@ const Filters = ({ search, name, toDate, fromDate, role, type, status, vendor, d
             })
         }
     }, [role])
+
+
+    useEffect(() => {
+        if (project_number) {
+            const response = fetchProjects()
+            response.then((res) => {
+                if (res?.data) {
+                    setProjects(res.data);
+                }
+            })
+        }
+    }, [project_number])
+
 
     useEffect(() => {
         if (name) {
@@ -68,6 +83,7 @@ const Filters = ({ search, name, toDate, fromDate, role, type, status, vendor, d
             filter_by_po_type: "",
             filter_by_po_vendor: "",
             filter_by_po_status: "",
+            filter_by_project_number: "",
             filter_by_name: "",  // By creator name
             filter_by_role: "",
             to_date: "",
@@ -126,33 +142,33 @@ const Filters = ({ search, name, toDate, fromDate, role, type, status, vendor, d
                 </div>} */}
 
                     <div>
-                    {toDate && (
-                        <div className="date-wrapp me-1">
-                            <label htmlFor="">{query.to_date || "To Date"}</label>
-                            <input
-                                type="date"
-                                className="input-date"
-                                placeholder=""
-                                onChange={(event) => {
-                                    const selectedDate = event.target.value;
-                                    const formattedDate = new Date(selectedDate).toISOString().split('T')[0];
-                                    if (query.from_date && selectedDate < query.from_date) {
-                                        setErrorMessage("To date cannot be smaller than from date");
-                                        return;
-                                    }
-                                    setErrorMessage("");
-                                    setQuery(prevState => ({
-                                        ...prevState,
-                                        to_date: formattedDate
-                                    }));
-                                }}
-                                value={query['to_date']}
-                            />
-                        </div>
-                    )}
-                    {errorMessage && (
-                        <div className="error">{errorMessage}</div>
-                    )}
+                        {toDate && (
+                            <div className="date-wrapp me-1">
+                                <label htmlFor="">{query.to_date || "To Date"}</label>
+                                <input
+                                    type="date"
+                                    className="input-date"
+                                    placeholder=""
+                                    onChange={(event) => {
+                                        const selectedDate = event.target.value;
+                                        const formattedDate = new Date(selectedDate).toISOString().split('T')[0];
+                                        if (query.from_date && selectedDate < query.from_date) {
+                                            setErrorMessage("To date cannot be smaller than from date");
+                                            return;
+                                        }
+                                        setErrorMessage("");
+                                        setQuery(prevState => ({
+                                            ...prevState,
+                                            to_date: formattedDate
+                                        }));
+                                    }}
+                                    value={query['to_date']}
+                                />
+                            </div>
+                        )}
+                        {errorMessage && (
+                            <div className="error">{errorMessage}</div>
+                        )}
                     </div>
                     {/* {toDate && <div class="date-wrapp me-1"> <label for="">{query.to_date || "To Date"}</label>
                     <input type="date" class="input-date" placeholder=""
@@ -235,6 +251,29 @@ const Filters = ({ search, name, toDate, fromDate, role, type, status, vendor, d
                         )
                         )}
                     </Select>}
+
+
+                    {project_number && <Select className="line-select dropdown-report me-2" placeholder="Role"
+                        onChange={(value) =>
+                            setQuery(prevState => ({
+                                ...prevState,
+                                ['filter_by_project_number']: value
+                            }))}
+                        value={query['filter_by_project_number'] || "Project No."}
+                    >
+                        {projects.map((project) =>
+                        (
+                            <Select.Option key={project.project_no} value={project.project_no}
+                            >
+                                {project.project_no}
+                            </Select.Option>
+                        )
+                        )}
+
+                    </Select>}
+
+
+
                     {status && <Select className="line-select  me-2" placeholder="PO Status"
                         onChange={(value) =>
                             setQuery(prevState => ({
